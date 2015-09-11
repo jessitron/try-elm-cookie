8c632489e1842e777336718df80f79c0faff7cb2
Jessica Kerr <jessitron@outpace.com>
2015-09-11 03:27:48 +0000

    remove logs. and how did this work with fieldnames wrong?

--- src/Native/Cookie.js
+++ src/Native/Cookie.js
@@ -11,19 +11,12 @@ Elm.Native.Cookie.make = function(localRuntime) {
   var Task = Elm.Native.Task.make(localRuntime);
   var Maybe = Elm.Maybe.make(localRuntime);
 
-  console.log("what it is here " + localRuntime);
-
-  console.log("what is the document " + document);
-  console.log("what is the document cookie <" + document.cookie + ">");
-
-
   var howToGetACookie = function(sKey) {
     return decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null;
   }
 
   function set(co) 
   {
-    console.log("set triggered")
     key = co.key
     value = co.value
     var setcommand = encodeURIComponent(key) + "=" + encodeURIComponent(value)
@@ -31,24 +24,19 @@ Elm.Native.Cookie.make = function(localRuntime) {
     var newValue = howToGetACookie(key)
     if (newValue === value)
     {
-      return Task.succeed({k: key, v: value});  
+      return Task.succeed({key: key, value: value});  
     } else {
       return Task.fail("cookie " + setcommand + " was not set. It holds <" + newValue + ">")
     }
-   
   }
 
   function get(key)
   {
-    console.log ("get triggered")
     var value = howToGetACookie(key)
     if (!value) {
       return Task.succeed(Maybe.Nothing);
     } else {
       var output = {key: key, value: value};
-      console.log("returning: " + output)
-      console.log("returning: k" + output.key)
-console.log("returning: v" + output.value)
       return Task.succeed(Maybe.Just(output));
     }
   }   
